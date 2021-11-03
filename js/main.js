ba8331b01e338e44ab5183960a9c09926222fc90
AliJabrailzade <jabrailzadeali@gmail.com>
2021-11-03 14:18:07 +0400

    Added js and css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 const blogs = document.querySelector('#blogs');
+const hamburgerMenu = document.querySelector('.hamburger-menu-icon');
+const hamburgerMenuLinks = document.querySelector('.hamburger-menu-links');
 
 fetch('http://localhost:5000/posts')
 	.then(response => response.json())
@@ -10,11 +12,21 @@ fetch('http://localhost:5000/posts')
 			const h3 = document.createElement('h3');
 			const p = document.createElement('p');
 
-			console.log(data['author'])
+			console.log(data['author']);
 			h1.innerHTML = data.title;
 			h3.innerHTML = data.author;
 			p.innerHTML = data.content;
-			div.append(h1, h3, p)
-			blogs.append(div)
+			div.append(h1, h3, p);
+			blogs.append(div);
 		})
 	});
+
+hamburgerMenu.onclick = () => {
+	const menuStyle = hamburgerMenuLinks.style.display;
+	if (menuStyle === 'none') {
+		hamburgerMenuLinks.style.display = 'block'
+	} else {
+		hamburgerMenuLinks.style.display = 'none'
+	}
+}
+	
